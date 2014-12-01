13ad3f6a271716677d40af5e308b9299dbb14b29
pieper126 <stijnbijnen@hotmail.com>
2014-12-01 19:24:02 +0100

    updated opdracht 1

--- AV/opdracht1/src/Nodes/BinaryNode.java
+++ AV/opdracht1/src/Nodes/BinaryNode.java
@@ -5,20 +5,48 @@
  */
 package Nodes;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author pieper126
  */
 public class BinaryNode extends Node {
+
     protected Node sideA;
     protected Node sideB;
-    
+
     public BinaryNode(Node sideA, Node sideB) {
 //        String stringSideA = super.currentEquation.equation.substring(0, super.currentEquation.equation.indexOf(",") - 1); // meerder comma's mogelijk
 //        String stringSideB = super.currentEquation.equation.substring(super.currentEquation.equation.indexOf(",") + 1);
-        
+
         this.sideA = sideA;
         this.sideB = sideB;
     }
-    
+
+    @Override
+    public List<Node> getDistinctVariable() {
+        ArrayList<Node> returnvalue = new ArrayList<Node>();
+
+        // test if sideA is already a Abstract variable
+        if (sideA.getClass().getTypeName() == AbstractVariable.class.getTypeName()) {
+            if (!returnvalue.contains(sideA.toString())) {
+                returnvalue.add(sideA);
+            }
+        } else {
+            returnvalue.addAll((ArrayList<Node>)sideA.getDistinctVariable());
+        }
+
+        // test if sideB is already a Abstract variable
+        if (sideB.getClass().getTypeName() == AbstractVariable.class.getTypeName()) {
+            if (!returnvalue.contains(sideB.toString())) {
+                returnvalue.add(sideB);
+            }
+        } else {
+            returnvalue.addAll((ArrayList<Node>)sideB.getDistinctVariable());
+        }
+        
+        return returnvalue;
+    }
 }
--- AV/opdracht1/src/Nodes/Node.java
+++ AV/opdracht1/src/Nodes/Node.java
@@ -1,7 +1,14 @@
 package Nodes;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class Node {
 
     public Node() {
+    }
+
+    public List<Node> getDistinctVariable() {
+        return null;
     }
 }
