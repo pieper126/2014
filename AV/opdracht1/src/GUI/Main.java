ce1f21bd14dfc5ff61fa513b3bc2272211175fe3
pieper126 <stijnbijnen@hotmail.com>
2015-01-05 23:00:00 +0100

    updated ui

--- AV/opdracht1/src/GUI/Main.java
+++ AV/opdracht1/src/GUI/Main.java
@@ -20,59 +20,65 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-//        Console console = System.console();
-//
-//        String exit;
-//        do {
-//            exit = console.readLine("Enter equation in prefix: ");
-//
-//            Equation equation = new Equation(exit);
-//
-//            String whatToDo = "";
-//            do {
-//                whatToDo = console.readLine("\"normal\" for the normal truthtable \n \"simplified\" for the simplified truthtable \n \"disjunctive normal form\" for the disjunctie normal form \n \"equality\" to check if a another equation is equal \n \"exit\" to exit the application \n"
-//                        + "enter a option: ");
-//
-//                switch (whatToDo) {
-//                    case "help":
-//                        System.out.print("\"normal\" for the normal truthtable \n \"simplified\" for the simplified truthtable \n \"disjunctive normal form\" for the disjunctie normal form \n \"equality\" to check if a another equation is equal \n \"exit\" to exit the application");
-//                        break;
-//                    case "normal":
-//                        long timer = System.currentTimeMillis();
-//                        equation.printTruthTable();
-//                        timer = System.currentTimeMillis() - timer;
-//                        System.out.println("time it took to complete: " + timer + "ms \n");
-//                        break;
-//                    case "simplified":
-//                        timer = System.currentTimeMillis();
-//                        equation.printingSimplifiedTruthTables();
-//                        timer = System.currentTimeMillis() - timer;
-//                        System.out.println("time it took to complete: " + timer + "ms \n");
-//                        break;
-//                    case "disjunctive normal form":
-//                        timer = System.currentTimeMillis();
-//                        equation.printDisjunctiveNormalForm();
-//                        timer = System.currentTimeMillis() - timer;
-//                        System.out.println("time it took to complete: " + timer + "ms \n");
-//                        break;
-//                    case "equality":
-//                        String newEquation = console.readLine("Enter equation in prefix: ");                        
-//                        timer = System.currentTimeMillis();
-//                        try{
-//                        if(equation.equals(new Equation(newEquation))){
-//                            System.out.println("they are equal");
-//                        } else{
-//                            System.out.println("they are not equal");
-//                        }
-//                        } catch (NullPointerException e){
-//                            System.out.println("run simplified first!");
-//                        }
-//                        timer = System.currentTimeMillis() - timer;
-//                        System.out.println("time it took to complete: " + timer + "ms \n");
-//                        break;
-//                }
-//            } while (!whatToDo.trim().toLowerCase().equals("exit"));
-//        } while (!exit.trim().toLowerCase().equals("exit"));
+        Console console = System.console();
+
+        String exit;
+        do {
+            exit = console.readLine("Enter equation in prefix: ");
+
+            Equation equation = new Equation(exit);
+
+            String whatToDo = "";
+            do {
+                whatToDo = console.readLine("\"1\" for the normal truthtable \n \"2\" for the simplified truthtable \n \"3\" for the disjunctie normal form \n \"4\" to check if a another equation is equal \n \"5\" to give the nand form of this equation \n \"exit\" to exit the application \n"
+                        + "enter a option: ");
+
+                switch (whatToDo) {
+                    case "0":
+                        System.out.print("\"normal\" for the normal truthtable \n \"simplified\" for the simplified truthtable \n \"disjunctive normal form\" for the disjunctie normal form \n \"equality\" to check if a another equation is equal \n \"exit\" to exit the application");
+                        break;
+                    case "1":
+                        long timer = System.currentTimeMillis();
+                        equation.printTruthTable();
+                        timer = System.currentTimeMillis() - timer;
+                        System.out.println("time it took to complete: " + timer + "ms \n");
+                        break;
+                    case "2":
+                        timer = System.currentTimeMillis();
+                        equation.printingSimplifiedTruthTables();
+                        timer = System.currentTimeMillis() - timer;
+                        System.out.println("time it took to complete: " + timer + "ms \n");
+                        break;
+                    case "3":
+                        timer = System.currentTimeMillis();
+                        equation.printDisjunctiveNormalForm();
+                        timer = System.currentTimeMillis() - timer;
+                        System.out.println("time it took to complete: " + timer + "ms \n");
+                        break;
+                    case "4":
+                        String newEquation = console.readLine("Enter equation in prefix: ");
+                        timer = System.currentTimeMillis();
+                        try {
+                            if (equation.equals(new Equation(newEquation))) {
+                                System.out.println("they are equal");
+                            } else {
+                                System.out.println("they are not equal");
+                            }
+                        } catch (NullPointerException e) {
+                            System.out.println("run simplified first!");
+                        }
+                        timer = System.currentTimeMillis() - timer;
+                        System.out.println("time it took to complete: " + timer + "ms \n");
+                        break;
+                    case "5":
+                        timer = System.currentTimeMillis();
+                        equation.printNAND();
+                        timer = System.currentTimeMillis() - timer;
+                        System.out.println("time it took to complete: " + timer + "ms \n");
+                        break;
+                }
+            } while (!whatToDo.trim().toLowerCase().equals("exit"));
+        } while (!exit.trim().toLowerCase().equals("exit"));
 
         // TODO code application logic here
 //        Equation test = new Equation("=( >(D,B), &( ~(A) ,C) )");
@@ -82,14 +88,13 @@ public class Main {
 //        Equation test = new Equation("=(C,B)");
 //        Equation test = new Equation("|(|(|(|(|(|(|(|(A,B),C),D),E),F),G),H),I)");
 //        Equation test = new Equation("A");
-        Equation test = new Equation("~(A)");
-        
+//        Equation test = new Equation("~(A)");
 //>(A,B)
 //=(A,B)
 //&(A,B)
 //|(A,B)
-        test.printTruthTable();
-        test.printNAND();
+//        test.printTruthTable();
+//        test.printNAND();
 //        test2.printTruthTable();
 //
 //        test.printingSimplifiedTruthTables();
@@ -97,8 +102,6 @@ public class Main {
 //        test.printDisjunctiveNormalForm();
 //        
 //        System.out.println(test.equals(test2));
-        
-        
     }
 
     public static void test() {
